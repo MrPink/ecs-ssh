3e4d4cb9ff3663596569b4e827566da1a4221ca6
MrPink <will.pink@gmail.com>
2018-05-04 13:24:38 +0000

    chore: fix entrypoint and dependency

--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ setup(
     # There are some restrictions on what makes a valid project name
     # specification here:
     # https://packaging.python.org/specifications/core-metadata/#name
-    name='ecs-ssh',  # Required
+    name='ecs_ssh',  # Required
 
     # Versions should comply with PEP 440:
     # https://www.python.org/dev/peps/pep-0440/
@@ -40,7 +40,7 @@ setup(
     # For a discussion on single-sourcing the version across setup.py and the
     # project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='1.0.2',  # Required
+    version='1.0.3',  # Required
 
     # This is a one-line description or tagline of what your project does. This
     # corresponds to the "Summary" metadata field:
@@ -95,14 +95,16 @@ setup(
     #
     packages=find_packages(exclude=['contrib', 'docs', 'tests']),  # Required
 
+    install_requires=[
+        'boto3',
+    ],
+
     extras_require={  # Optional
         'dev': ['check-manifest'],
         'test': ['coverage'],
     },
 
     entry_points={  # Optional
-        'console_scripts': [
-            'ecs-ssh = ecs_ssh:main',
-        ],
+        'console_scripts': ['ecs-ssh=ecs_ssh.ecs_ssh:main'],
     },
 )
